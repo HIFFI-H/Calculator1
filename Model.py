71f771e9251f3c5d07349edc0d221248ea071048
Hafiz Irfan Ali <irfanali0118be@gmail.com>
2025-08-22 23:44:04 +0500

    Update Model.py

--- Model.py
+++ Model.py
@@ -2,6 +2,7 @@
 
 import numpy as np
 import pandas as pd
+import seaborn as sns
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, confusion_matrix
